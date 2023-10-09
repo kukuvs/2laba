b74c12fda6f6295f23e91f5b85eae5ac0273c966
kukuvs <danil.bakhmetev05@gmail.com>
2023-10-10 00:37:41 +0400

    3

--- fgh.py
+++
@@ -1,9 +0,0 @@
-
-mas = [90,2,3,5,4,1,7,6,0,9,8]
-n = len(mas)
-
-for run in range(n-1):
-    for i in range(n-1):
-        if mas[i] > mas[i+1]:
-            mas[i], mas[i+1] = mas[i+1], mas[i]
-print(mas)
